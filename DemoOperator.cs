2c22287764f7e9ea51004ba2a74bc52aebc50db4
yudits10 <113966685+yudits10@users.noreply.github.com>
2022-09-20 13:39:55 +0700

    Add files via upload

---
+++ DemoOperator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DemoOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 20;
+            var b = 9;
+            var d = 7;
+
+            var e = (a + b) * d;
+            //Console.WriteLine(a + b);
+            //Console.WriteLine(a > b);
+            //Console.WriteLine(a == b);
+            //Console.WriteLine(a != b);
+
+            //Console.WriteLine( !(a != b));
+
+            Console.WriteLine(d > b && d > a);
+            Console.WriteLine(a > b || d == a);
+            Console.WriteLine(a > b && a > d);
+        }
+    }
+}
